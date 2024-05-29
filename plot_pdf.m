4f1bf33ef44860883a6c622effd69e9dd75789bb
ling-wyj <47814487+Ling-WYJ@users.noreply.github.com>
2024-05-29 16:35:52 +0800

    Split arrow

--- plot_pdf.m
+++ plot_pdf.m
@@ -5,7 +5,7 @@ function plot_pdf(pns)
     arcs = pns.incidence_matrix;
     
     % 创建图形窗口
-    figure;
+    figure('units','normalized','outerposition',[0 0 1 1], 'Color', 'w');
     hold on;
 
     % 绘制库所
@@ -16,7 +16,7 @@ function plot_pdf(pns)
         angle = 2 * pi * (i-1) / num_places; % 计算每个库所的角度位置
         place_positions(i, :) = radius * [cos(angle), sin(angle)];
         plot(place_positions(i, 1), place_positions(i, 2), 'o', 'MarkerSize', 20, 'MarkerFaceColor', 'w');
-        text(place_positions(i, 1) + 1, place_positions(i, 2), places(i).name, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle'); 
+        text(place_positions(i, 1) -0.5, place_positions(i, 2)-1, places(i).name, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle'); 
     end
 
     % 绘制变迁
@@ -32,7 +32,7 @@ function plot_pdf(pns)
         x = transition_positions(i, 1) - width / 2;
         y = transition_positions(i, 2) - height / 2;
         rectangle('Position', [x, y, width, height], 'FaceColor', 'w');
-        text(transition_positions(i, 1) -0.2, transition_positions(i, 2)+ 1.3, transitions(i).name, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
+        text(transition_positions(i, 1)-0.2 , transition_positions(i, 2)+ 1.3, transitions(i).name, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
     
     end
 
@@ -50,62 +50,10 @@ function plot_pdf(pns)
             else
                 if arcs(i, j) ~= 0
                     start_pos = transition_positions(i, :);
-                    end_pos = place_positions(j-num_places, :);
+                    end_pos = place_positions(j - num_places, :);
                     draw_arrow(start_pos, end_pos, arcs(i,j));
                 end
            end
           
         end
     end
-
-
-
-     % 设置图形属性
-    axis equal;
-    axis([-radius-5, radius+5, -radius-5, radius+5]); % 设置绘图范围
-    set(gca, 'XTick', []);
-    set(gca, 'YTick', []);
-    title('Petri Net Visualization');
-    hold off;
-
-end
-
-function draw_arrow(start_pos, end_pos, weight)
-    % 计算箭头的方向向量
-    direction = end_pos - start_pos;
-    
-    % 可以标准化方向向量以使所有箭头大小一致
-    norm_direction = direction / norm(direction);
-    
-    % 设置箭头的大小
-    arrowSize = 0.5; % 可以调整箭头大小
-    headWidth = arrowSize; % 箭头的宽度
-    headLength = arrowSize; % 箭头的长度
-    
-    % 计算箭头尖端和两侧翼尖的位置
-    arrowTip = end_pos;
-    arrowLeft = end_pos - headLength * norm_direction + ...
-                headWidth / 2 * [-norm_direction(2), norm_direction(1)];
-    arrowRight = end_pos - headLength * norm_direction - ...
-                headWidth / 2 * [-norm_direction(2), norm_direction(1)];
-    
-    % 绘制箭头线条部分
-    line([start_pos(1), end_pos(1)], ...
-         [start_pos(2), end_pos(2)], 'Color', 'k', 'LineWidth', 1);
-    
-    % 绘制实心三角形箭头
-    patch('XData', [arrowLeft(1), arrowTip(1), arrowRight(1)], ...
-          'YData', [arrowLeft(2), arrowTip(2), arrowRight(2)], ...
-          'FaceColor', 'k', 'EdgeColor', 'k');
-    
-    % 计算弧线中点的位置
-    mid_pos = (start_pos + end_pos) / 2;
-    
-    % 在弧线中点显示权重
-    text(mid_pos(1), mid_pos(2), num2str(weight), ...
-        'HorizontalAlignment', 'center', ...
-        'VerticalAlignment', 'bottom', ...
-        'FontSize', 15, 'Color', 'r');
-end
-
-
